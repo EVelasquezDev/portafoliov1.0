5ed2fcc89760faa57accc0117cfcda5bfe471020
Elias Velasquez <evelasquez@eserviciosat.net>
2019-07-30 16:32:46 -0400

    Modificación banner tamaño de pantalla

--- banner.php
+++ banner.php
@@ -1,13 +1,15 @@
-	<header class="main-header" id="inicio">
-		<div class="background-overlay text-white py-6">
+	<header class="main-header img-responsive" id="inicio">
+		<div class="background-overlay text-white">
 			<div class="container">
-				<div class="row">
-					<div class="col text-center justify-content-center mt-5">
-						<h1>DEVELOPER JUNIOR</h1>
-						<h4><em>"Todos nuestros sueños se pueden hacer realidad si tenemos el coraje de perseguirlos"<em></h4>
-						<button id="btndatospersonales" class="btn btn-secondary btn-lg text-white mt-3 mb-3 rounded">
-							¡Conocer más!
-						</button>
+				<div class="row justify-content-center align-items-center minh-100">
+					<div class="col text-center">
+						<div class="info-banner">
+							<h1>DEVELOPER JUNIOR</h1>
+							<h4><em>"Todos nuestros sueños se pueden hacer realidad si tenemos el coraje de perseguirlos"<em></h4>
+							<button id="btndatospersonales" class="btn btn-secondary btn-lg text-white mt-3 mb-3 rounded">
+								¡Conocer más!
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
